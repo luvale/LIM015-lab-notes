b6bd52d5949bdc74910c6408f453606722098070
unknown <valeriae200@hotmail.com>
2024-01-16 18:50:53 -0500

    deleting notes works

--- labNotes/src/app/services/firestore.service.ts
+++ labNotes/src/app/services/firestore.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, addDoc, doc, updateDoc, getDocs, getDoc } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Firestore, collection, addDoc, doc, updateDoc, getDocs, getDoc, deleteDoc } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +10,25 @@ export class FirestoreService {
     private firestore: Firestore,
   ) { }
 
-  getMyNotes(userEmail:string) {
-    const placeRef = collection(this.firestore, 'notes-' + userEmail);
-    return getDocs(placeRef);
+  getMyNotes(userEmail: string) {
+    const collectionRef = collection(this.firestore, 'notes-' + userEmail);
+    return getDocs(collectionRef);
   }
-  getNoteById(userEmail:string, noteId:string) {
+  getNoteById(userEmail: string, noteId: string) {
     const docRef = doc(this.firestore, 'notes-' + userEmail, noteId);
     return getDoc(docRef);
   }
 
-  saveNote(userEmail:string, note:any) {
-    const placeRef = collection(this.firestore, 'notes-' + userEmail);
-    return addDoc(placeRef, note);
+  saveNote(userEmail: string, note: any) {
+    const docRef = collection(this.firestore, 'notes-' + userEmail);
+    return addDoc(docRef, note);
   }
-  updateNote(userEmail:string, note:any, noteId:string) {
+  updateNote(userEmail: string, note: any, noteId: string) {
     const docRef = doc(this.firestore, 'notes-' + userEmail, noteId);
     return updateDoc(docRef, note);
+  }
+  deleteNote(userEmail: string, noteId: string) {
+    const docRef = doc(this.firestore, 'notes-' + userEmail, noteId);
+    return deleteDoc(docRef);
   }
 }
--- labNotes/src/app/views/my-notes/my-notes.component.ts
+++ labNotes/src/app/views/my-notes/my-notes.component.ts
@@ -8,8 +8,8 @@ import { FirestoreService } from 'src/app/services/firestore.service';
   styleUrls: ['./my-notes.component.scss']
 })
 export class MyNotesComponent implements OnInit {
-  userEmail:string = '';
-  notes:any = [];
+  userEmail: string = '';
+  notes: any = [];
 
   constructor(
     private readonly firestoreService: FirestoreService,
@@ -22,15 +22,21 @@ export class MyNotesComponent implements OnInit {
     this.getMyNotes();
   }
   async getMyNotes() {
+    this.notes = [];
     const data = await this.firestoreService.getMyNotes(this.userEmail)
     data.forEach((doc) => {
       const content = {
-        id : doc.id,
+        id: doc.id,
         note_content: doc.data()['note_content'],
         note_title: doc.data()['note_title']
       }
       this.notes.push(content);
     });
     console.log(this.notes)
+  }
+  deleteNote(note: any) {
+    console.log(note.id);
+    this.firestoreService.deleteNote(this.userEmail, note.id);
+    this.getMyNotes();
   }
 }
